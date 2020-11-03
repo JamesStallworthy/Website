d426751e8aaf0e9dc3278fe0c445510f9ea507c6
JamesStallworthy <james@jamesstallworthy.com>
2020-11-03 21:47:08 +0000

    Implemented the projects page

---
+++ js/projects.js
@@ -0,0 +1,40 @@
+var icons = {
+    "C#":"./icons/csharp.svg",
+    "HTML": "./icons/html5.svg",
+    "C++": "./icons/C++.svg",
+    "C": "./icons/C++.svg",
+    "Python": "./icons/python.svg",
+    "Go": "./icons/go.svg",
+    "JavaScript": "./icons/javascript.svg"
+}
+
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        projects: []
+    }
+});
+
+var githubURL = 'https://api.github.com/users/jamesstallworthy/repos?sort=updated'
+
+$.ajax({
+    url: githubURL,
+    type: 'GET',
+    dataType: 'json', // added data type
+    success: function(res) {
+        for (var i = 0; i < res.length; i++){
+            if (!res[i].fork){
+                var proj = {
+                    name: res[i].name,
+                    desc: res[i].description,
+                    link: res[i].html_url,
+                    language: res[i].language,
+                    languageIcon: icons[res[i].language]
+                }
+
+                app.projects.push(proj)
+            }
+        }
+        $(".loading-text").remove()
+    }
+});
